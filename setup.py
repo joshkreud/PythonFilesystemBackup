7ee65c9edbc583642b198f042f37cec6c99f9078
Joshua K <yoshi199234@gmail.com>
2020-07-22 10:09:10 +0200

    up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="filesystembackup",
-    version="0.1",
+    version="0.1.1",
     description="Can extract files from directory, filter them and generate a ZIP",
     url="https://github.com/joshkreud/PythonFilesystemBackup",
     author="Joshua Kreuder",
@@ -10,5 +10,5 @@ setup(
     license="MIT",
     packages=["filesystembackup"],
     zip_safe=False,
-    install_requires=["pandas",],
+    install_requires=["pandas"],
 )
